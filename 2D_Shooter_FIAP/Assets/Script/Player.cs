31f5d77fe7cf2c22abfda09256affcce38ac83c7
Leonardo Hisao Watanabe Silva <leonardohwata@gmail.com>
2023-08-15 20:48:35 -0300

    Animation Controller

--- 2D_Shooter_FIAP/Assets/Script/Player.cs
+++ 2D_Shooter_FIAP/Assets/Script/Player.cs
@@ -11,14 +11,16 @@ public class Player : MonoBehaviour
 
     private Animator animator;
     private Transform playerTransform;
-    
-
+    private bool isMoving;
+    private bool isHurt;
 
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         playerTransform = GetComponent<Transform>();
+
+        animator = GetComponent<Animator> ();
 
         if (instance == null)
         {
@@ -31,13 +33,25 @@ public class Player : MonoBehaviour
     }
     private void Update()
     {
-        Move();
+        MoveHandler();
+        LifeHandler();
+        AnimatorHandler();
     }
 
-    private void Move()
+#region Handlers
+    private void MoveHandler()
     {
         float moveX = Input.GetAxisRaw("Horizontal") * velocity * Time.deltaTime;
         float moveY = Input.GetAxisRaw("Vertical") * velocity * Time.deltaTime;
+        isMoving = moveX != 0 || moveY != 0;
+        //if (moveX != 0 || moveY != 0)
+        //{
+        //    isMoving = true;
+        //}
+        //else
+        //{
+        //    isMoving = false;
+        //}
 
         playerTransform.Translate(new Vector3 (moveX, moveY, 0));
         
@@ -47,6 +61,36 @@ public class Player : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void LifeHandler()
+    {
+
+    }
+
+    private void AnimatorHandler()
+    {
+        bool isMovingAnimation = animator.GetBool("isMoving");
+        bool isHurtAnimation = animator.GetBool("isHurt");
+
+        if (isMoving && isMovingAnimation == false)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else if (!isMoving && isMovingAnimation == true)
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        if (isHurt && isHurtAnimation == false)
+        {
+            animator.SetBool("isHurt", true);
+            isHurt = false;
+        }
+        else if (!isHurt && isHurtAnimation == true)
+        {
+            animator.SetBool("isHurt", false);
+        }
+    }
+#endregion
 
     public Vector3 GetPlayerPosition()
     {
@@ -55,6 +99,7 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        isHurt = true;
         lives--;
     }
 }
